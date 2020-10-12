ba88e796adb00a81763f9c20637746953ff563aa
Brenson <brensonw300@gmail.com>
2020-10-12 07:55:06 -0700

    Looked through code fixing any bugs

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
+
+import axios from "axios";
 
 export const fetchShow = () => {
   return axios
     .get(
-        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then((res) => {
-          console.log("SV: fetchShow.js: fetchShow: response", res);
-          return res.data;
-        })
-        .catch((err) => {
-          console.log("SV: fetchShow.js: fetchShow: error", err.message);
-          return err;
-        });
-    };
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then((res) => {
+      console.log("SV: fetchShow.js: fetchShow: response", res);
+      return res;
+    })
+    .catch((err) => {
+      console.log("SV: fetchShow.js: fetchShow: error", err.message);
+      return err;
+    });
+};
 
     
